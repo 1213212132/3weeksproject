f88020c44d28d1811591c9203ae1c61b1bfc55cd
LJH <95152341+LeeJinHyuk002@users.noreply.github.com>
2023-06-08 17:30:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,4 +6,21 @@ $(function () {
         showControls: false,
         playOnlyIfVisible: true,
     });
+
+
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 600)
+    });
+
+
+    $(window).on('scroll', function () {
+        //스크롤량을 구함.
+        let sct = $(window).scrollTop();
+        // console.log(sct);
+        //sct > 1000 ? $('.to_top').fadeIn(1) : $('.to_top').fadeOut(0.1);
+
+        sct > 1000 ? $('.to_top').addClass('on') : $('.to_top').removeClass('on');
+
+
+    })
 })
